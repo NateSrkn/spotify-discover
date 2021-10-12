882cd5b02704acc22e3ea6bddd60efd49a1ad0ce
Nathan Sorkin <NateSrkn@gmail.com>
2021-10-11 22:10:11 -0400

    Updated image style to cover parent

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,7 +1,13 @@
 import { pullInitials } from "../util/helpers";
 export default function ImageComponent({ src, alt, height, width }) {
   return src ? (
-    <img src={src} alt={alt} height={height} width={width} />
+    <img
+      src={src}
+      alt={alt}
+      height={height}
+      width={width}
+      className="object-cover"
+    />
   ) : (
     <div className="filler-img">{pullInitials(alt)}</div>
   );
