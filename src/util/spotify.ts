bd4fb873dd63d61398fffe97f4245ffeeeb3fafb
Nathan Sorkin <NateSrkn@gmail.com>
2022-04-14 00:29:10 -0400

    sometimes im dumb

--- src/util/spotify.ts
+++ src/util/spotify.ts
@@ -1,10 +1,7 @@
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 const client_id = process.env.NEXT_PUBLIC_CLIENT_ID;
 const client_secret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
-const redirect_uri =
-  process.env.NODE_ENV === "development"
-    ? process.env.NEXT_PUBLIC_DEV_REDIRECT_URI
-    : process.env.NEXT_PUBLIC_REDIRECT_URI;
+const redirect_uri = process.env.NEXT_PUBLIC_REDIRECT_URI;
 const auth = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
 
 export async function refreshToken(token) {
